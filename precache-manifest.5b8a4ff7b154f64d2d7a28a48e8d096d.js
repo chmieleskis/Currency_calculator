7d5e088aae821871079bee319502346e606991c3
Michal Chmielewski <m.chmielewski@tut.by>
2021-01-11 20:53:25 +0100

    Updates

---
+++ precache-manifest.5b8a4ff7b154f64d2d7a28a48e8d096d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8bf3f622effeeb96d954e7213a51a9",
+    "url": "/Currency_calculator/index.html"
+  },
+  {
+    "revision": "e7807dee5d03d33aa871",
+    "url": "/Currency_calculator/static/css/main.f6c05544.chunk.css"
+  },
+  {
+    "revision": "6c6a564326b7129c9a5d",
+    "url": "/Currency_calculator/static/js/2.94f2c5f3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Currency_calculator/static/js/2.94f2c5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7807dee5d03d33aa871",
+    "url": "/Currency_calculator/static/js/main.09f5bb8f.chunk.js"
+  },
+  {
+    "revision": "90095ad1dd6a1f568b5a",
+    "url": "/Currency_calculator/static/js/runtime-main.466bdc60.js"
+  },
+  {
+    "revision": "baa58c3c16b76d6e733e5f658ec2a3f8",
+    "url": "/Currency_calculator/static/media/background.baa58c3c.png"
+  }
+]);
